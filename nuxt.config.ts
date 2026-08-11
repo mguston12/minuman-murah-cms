0cde3821746d478aacf7b52c49fbe596905a61ca
Michael <mguston12@github.com>
2026-08-11 21:13:29 +0700

    load bootstrap global

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -61,8 +61,17 @@ export default defineNuxtConfig({
         
         // Google Fonts
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i' }
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i' },
         
+        // Load Bootstrap CSS & Bootstrap Icons CDN
+        { 
+          rel: 'stylesheet', 
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' 
+        },
+        { 
+          rel: 'stylesheet', 
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css' 
+        }
       ]
     }
   },
